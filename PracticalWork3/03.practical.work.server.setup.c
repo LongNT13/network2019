6be266773e6e5bc206eb8f74d04ba67cbbed98e5
LongNT13 <tanguyenlongntt@gmail.com>
2019-03-19 20:47:54 +0700

    Upload pratical work 3

---
+++ PracticalWork3/03.practical.work.server.setup.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int sockfd, clen, clientfd;
+	struct sockaddr_in saddr, caddr;
+	unsigned short port = 8784;
+	if((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Error creating socket\n");
+		exit(-1);
+	}
+
+	memset(&saddr, 0,sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	saddr.sin_port = htons(port);
+	
+	if((bind(sockfd, (struct sockaddr *) &saddr,sizeof(saddr))) < 0) {
+		printf("Error binding\n");
+		exit(-1);
+	}
+
+	if(listen(sockfd, 5) < 0) {
+		printf("Error listening\n");
+		exit(-1);
+	}
+
+	//error
+	clen=sizeof(caddr);
+	if((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
+		printf("Error accepting connection\n");
+		exit(-1);
+	}else{
+		printf("successfully connected\n");
+	}
+
+	return 0;
+
+}
